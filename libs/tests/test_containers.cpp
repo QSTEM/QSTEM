e94e44414385cecb7761b7392276884099bfb1c5
Michael Sarahan <msarahan@gmail.com>
2013-06-20 09:44:14 -0700

    Adding tests for libs.  Trying to make CMake run tests.

--- libs/tests/test_containers.cpp
+++ libs/tests/test_containers.cpp
@@ -1,10 +1,24 @@
+#define BOOST_TEST_MODULE Hello
+#include <boost/test/unit_test.hpp>
+ 
+int add(int i, int j)
+{
+    return i + j;
+}
+ 
+BOOST_AUTO_TEST_CASE(universeInOrder)
+{
+    BOOST_CHECK(add(2, 2) == 4);
+}
+
+/*
 #define BOOST_TEST_MODULE "TestContainers"
 //#define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
 //#include "data_containers.h"
 #include <iostream>
 
-/*
+
 struct WaveFixture {
     WaveFixture() 
 	{ 
@@ -28,7 +42,7 @@ struct DetectorFixture {
 
     DetectorPtr det;
 };
-*/
+
 
 int add(int i, int j)
 {
@@ -42,7 +56,7 @@ BOOST_AUTO_TEST_CASE(universeInOrder)
     BOOST_CHECK(add(2, 2) == 4);
 }
 BOOST_AUTO_TEST_SUITE_END( )
-/*
+
 BOOST_FIXTURE_TEST_SUITE (TestWave, WaveFixture) // name of the test suite is wavetest
 
 BOOST_AUTO_TEST_CASE (testArrayAllocation)
