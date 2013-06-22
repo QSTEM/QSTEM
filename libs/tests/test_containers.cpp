0589671574169694a09176ed923782902cfac2a5
Michael Sarahan <msarahan@gmail.com>
2013-06-21 22:41:28 -0700

    got basic memory allocation tests working - test framework should now be fully functional.

--- libs/tests/test_containers.cpp
+++ libs/tests/test_containers.cpp
@@ -3,72 +3,38 @@
 #include "data_containers.h"
 #include <iostream>
 
-int add(int i, int j)
-{
-    return i + j;
-}
- 
-BOOST_AUTO_TEST_SUITE(Maths)
- 
-BOOST_AUTO_TEST_CASE(universeInOrder)
-{
-    BOOST_CHECK(add(2, 2) == 4);
-}
-
-BOOST_AUTO_TEST_SUITE_END( )
-
-BOOST_AUTO_TEST_SUITE(Maths1)
- 
-BOOST_AUTO_TEST_CASE(universeInOrder)
-{
-    BOOST_CHECK(add(2, 2) == 4);
-}
-
-BOOST_AUTO_TEST_SUITE_END( )
-
-BOOST_AUTO_TEST_SUITE(Maths2)
- 
-BOOST_AUTO_TEST_CASE(universeInOrder)
-{
-    BOOST_CHECK(add(2, 2) == 5);
-}
-
-BOOST_AUTO_TEST_SUITE_END( )
-
-
 struct WaveFixture {
-    WaveFixture() 
-	{ 
-		std::cout << "setup" << std::endl; 
-		WavePtr wave = WavePtr( new WAVEFUNC(10, 10, 0.2f, 0.2f));
-	}
-    ~WaveFixture()
-	{ std::cout << "teardown" << std::endl; }
+  WaveFixture():
+    wave(WavePtr( new WAVEFUNC(10, 10, 1.0, 1.0)))
+  { 
+    std::cout << "setup wave fixture" << std::endl; 
+  }
+  ~WaveFixture()
+  { std::cout << "teardown wave fixture" << std::endl; }
 
-    WavePtr wave;
+  WavePtr wave;
 };
 
 struct DetectorFixture {
-    DetectorFixture() 
-	{ 
-		std::cout << "setup" << std::endl; 
-		DetectorPtr det = DetectorPtr( new Detector(10, 10, 0.2f, 0.2f));
-	}
-    ~DetectorFixture()
-	{ std::cout << "teardown" << std::endl; }
+  DetectorFixture() :
+    det(DetectorPtr(new Detector(10, 10, 0.2f, 0.2f)))
+  { 
+    std::cout << "setup detector fixture" << std::endl; 
+  }
+  ~DetectorFixture()
+  { std::cout << "teardown detector" << std::endl; }
 
-    DetectorPtr det;
+  DetectorPtr det;
 };
-/*
-BOOST_FIXTURE_TEST_SUITE (TestWave, WaveFixture) // name of the test suite is wavetest
+
+BOOST_FIXTURE_TEST_SUITE (TestWave, WaveFixture)
 
 BOOST_AUTO_TEST_CASE (testArrayAllocation)
 {
   // Check array allocation
-  //BOOST_CHECK(wave->diffpat != NULL);
-  //BOOST_CHECK(wave->avgArray != NULL);
-  //BOOST_CHECK(wave->wave != NULL);
-	BOOST_CHECK(true);
+  BOOST_CHECK(wave->diffpat != NULL);
+  BOOST_CHECK(wave->avgArray != NULL);
+  BOOST_CHECK(wave->wave != NULL);
 }
 
 // Test image saving
@@ -78,7 +44,7 @@ BOOST_AUTO_TEST_CASE (testArrayAllocation)
 BOOST_AUTO_TEST_SUITE_END( )
 
 
-BOOST_FIXTURE_TEST_SUITE (TestDetector, DetectorFixture) // name of the test suite is wavetest
+BOOST_FIXTURE_TEST_SUITE (TestDetector, DetectorFixture)
 
 BOOST_AUTO_TEST_CASE (testArrayAllocation)
 {
@@ -93,4 +59,3 @@ BOOST_AUTO_TEST_CASE (testSetComment)
 }
 
 BOOST_AUTO_TEST_SUITE_END( )
-*/
