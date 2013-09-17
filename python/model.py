a9051d33fb8ff079c99d18d339476d80f834dc13
Michael Sarahan <msarahan@gmail.com>
2013-09-16 21:12:20 -0700

    Implement rectangular cell duplication in model.py

--- python/model.py
+++ python/model.py
@@ -21,8 +21,9 @@ QSTEM - image simulation for TEM/STEM/CBED
 
 import numpy as np
 from traits.api import HasTraits, Dict, Float, Tuple, Int, Property, \
-     cached_property, Array, Bool
+     cached_property, Array, Bool, on_trait_change
 import readCfg
+import copy
 
 class SampleModel(HasTraits):
     # elements is a dictionary representing the structure.
@@ -34,9 +35,9 @@ class SampleModel(HasTraits):
     #          a fifth column for occupancy,
     #          a sixth column for site-specific charge
     elements = Dict
-    # this is a mirror of the elements dict, but includes any applied sample 
-    #    tilts.
-    transformed_elements = Dict
+    scaled_elements = Property(depends_on="elements",cached=True)
+    duplicated_elements = Property(depends_on="scaled_elements",cached=True)
+    transformed_elements = Property(depends_on="duplicated_elements",cached=True)
     nCellsX = Int(4)
     nCellsY = Int(4)
     nCellsZ = Int(4)
@@ -51,16 +52,56 @@ class SampleModel(HasTraits):
     usingNCells = Bool(True) # when true, computes supercell as multiple of 
     #                           cells.  When false, fills boxSize with 
     #                           duplicated cells.
-    
-    @on_trait_change("nCellsX, nCellsY, nCellsZ, elements")
-    def duplicateCell(self):
-        pass
         
-    @on_trait_change("tiltX, tiltY, tiltZ, elements")
-    def tiltCell(self, x, y, z):
-        from numpy import cos, sin
+    def loadCfg(self,filename):
+        self.cellSize=readCfg.get_cell_box(filename)
+        self.elements=readCfg.get_atom_arrays(filename)
+    
+    def _get_scaled_elements(self):
+        """
+        Translate fractional coordinates into real-space coordinates
+        """
+        scaled_elements={}
+        for key in self.elements.keys():
+            scaled_elements[key]=copy.copy(self.elements[key])
+            scaled_elements[key][:,:3]*=self.cellSize
+        return scaled_elements    
+    
+    def _get_duplicated_elements(self):
+        """
+        Duplicates elements according to your desired number of cells.
+        """
+        return self._duplicateCell(self.nCellsX, self.nCellsY, self.nCellsZ)
+
+    def _get_transformed_elements(self):
         if not self.tiltInDegrees:
             pass
+        return self._tiltCell(self.tiltX, self.tiltY, self.tiltZ)
+        
+    def _duplicateCell(self, nx, ny, nz):
+        duplicated_elements={}
+        for key in self.elements.keys():
+            num_repeats = nx*ny*nz
+            dup_array = np.tile(self.scaled_elements[key], [num_repeats,1])
+            num_entries = self.scaled_elements[key].shape[0]
+            ctr=0
+            for x in range(nx):
+                for y in range(ny):
+                    for z in range(nz):
+                        offset=ctr*num_entries
+                        cell_offset = np.array([x*self.cellSize[0], 
+                                                y*self.cellSize[1], 
+                                                z*self.cellSize[2],
+                                                0,0,0])
+                        dup_array[offset:offset+num_entries]+=cell_offset
+                        ctr += 1
+            duplicated_elements[key]=dup_array
+        return duplicated_elements
+                
+    def _tiltCell(self, x, y, z):
+        from numpy import cos, sin
+        transformed_elements={}
+        
         Rx = np.array([[1,0,0],
                        [0, cos(x), -sin(x)],
                        [0, sin(x), cos(x)]])
@@ -70,10 +111,13 @@ class SampleModel(HasTraits):
         Rz = np.array([[cos(z),-sin(z),0],
                        [sin(z), cos(z),0],
                        [0, 0, 1]])
-        for key in self.elements.keys():
-            self.transformed_elements[key]=self.elements[key].dot(Rx).dot(Ry).dot(Rz)
-        
-    def loadCfg(self,filename):
-        self.cellSize=readCfg.get_cell_box(filename)
-        self.elements=readCfg.get_atom_arrays(filename)
-    
+        for key in self.duplicated_elements.keys():
+            transformed_elements[key]=self.duplicated_elements[key].dot(Rx).dot(Ry).dot(Rz)
+        return transformed_elements
+            
+    def _get_element_arrays(self):
+        """
+        This is an alias for the transformed arrays, which represent the structure that has
+        been scaled, duplicated, and rotated as specified.
+        """
+        return self.transformed_elements
