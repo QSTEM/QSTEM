295098124e4e135722778c8a23b68aebde909a7c
Michael Sarahan <msarahan@gmail.com>
2013-10-04 19:29:15 -0700

    Cleanup some button names and get rid of extraneous files.

--- python/QSTEM_UI.py
+++ python/QSTEM_UI.py
@@ -19,54 +19,14 @@ QSTEM - image simulation for TEM/STEM/CBED
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 """
-from pyface.qt import QtCore,QtGui
-import sys
-from dialogs import main_window
 
 from pyface.util.guisupport import get_app_qt4, start_event_loop_qt4
 
-from models.sample_chaco import SamplePlotter
+from dialogs.main_window import QSTEM_UI
 
-class QSTEM_UI(QtGui.QMainWindow,main_window.Ui_MainWindow):
-    """
-    hwl is inherited from both QtGui.QDialog and hw.Ui_Dialog
-    """
-    def __init__(self,parent=None):
-        """
-        Initialization of the class. Call the __init__ for the super classes
-        """
-        super(QSTEM_UI,self).__init__(parent)
-        self.setupUi(self)
-        self.sample=SamplePlotter(self)
-        self.plotLayout.addWidget(self.sample.widget)
-        self.sampleWidget=self.sample.widget
-        self.sampleWidget.show()
-        self.connectActions()
-
-    def main(self):
-        self.show()
-
-    def connectActions(self):
-        """
-        Connect the user interface controls to the logic
-        """
-        self.LoadModel.clicked.connect(self.sample.load_file)
-
-        '''
-        def myprint(self):
-        """
-        Even handler for the pushButton click
-        """
-        self.txtLine.setText('Python -- ')        
-        self.txtEdit.setText('This')
-        self.lblShow.setText('is a test')
-        '''
-        
-    
-if __name__=='__main__':
+if __name__ == '__main__':
     #app = QtGui.QApplication(sys.argv)
     app = get_app_qt4()
     gui = QSTEM_UI()
     gui.main()
     start_event_loop_qt4(app)
-    #sys.exit(app.exec_())
