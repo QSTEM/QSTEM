4cc50bf701d0b4c994d059974a64481f19e66e5b
Michael Sarahan <msarahan@gmail.com>
2013-11-10 16:12:00 -0800

    revert removal of muls.  Premature - we'll get there...

---
+++ libs/data_containers.cpp
@@ -0,0 +1,78 @@
+/*
+QSTEM - image simulation for TEM/STEM/CBED
+    Copyright (C) 2000-2010  Christoph Koch
+	Copyright (C) 2010-2013  Christoph Koch, Michael Sarahan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "stdio.h"
+#include <string.h>
+#include "data_containers.hpp"
+#include "memory_fftw3.hpp"
+
+Detector::Detector(int nx, int ny, float_tt resX, float_tt resY) :
+  error(0),
+  shiftX(0),
+  shiftY(0),
+  Navg(0),
+  dx(resX),
+  dy(resY)
+{
+  image = float2D(nx,ny,"ADFimag");
+  image2 = float2D(nx,ny,"ADFimag");
+  
+  // TODO: need way of passing file formats into constructor here
+  m_imageIO=ImageIOPtr(new CImageIO(nx, ny));
+}
+
+void Detector::WriteImage(const char *fileName, const char *comment, std::map<std::string, double> &params,
+                          std::vector<unsigned> position)
+{
+  params["dx"]=dx;
+  params["dy"]=dy;
+  // Thickness is set externally and passed in on params
+  m_imageIO->WriteRealImage((void **)image, fileName, params, std::string(comment), position);
+}
+
+MULS::MULS():
+  cubex(0), cubey(0), cubez(0),
+  printLevel(2),
+  saveLevel(0),
+  lpartl(0),
+  atomRadius(5.0),
+  saveFlag(0),
+  sigmaf(0),
+  dfdelt(0),
+  acmax(0),
+  acmin(0),
+  aobj(0),
+  Cs(0),
+  aAIS(0),
+  tomoTilt(0),
+  tomoStart(0),
+  tomoStep(0),
+  tomoCount(0),
+  trans(NULL),
+  cz(NULL),
+  normHolog(0),
+  gaussianProp(0),
+  kx(NULL), kx2(NULL), ky(NULL), ky2(NULL),
+  pendelloesung(NULL),
+  onlyFresnel(NULL),
+  showPhaseplate(NULL),
+  czOffset(NULL)
+{
+
+}
