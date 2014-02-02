ea577fa22257f0fac7d4eee278b76f49abee6fc4
Michael Sarahan <msarahan@gmail.com>
2014-02-02 13:17:00 -0800

    add function for appending position to filename where necessary

---
+++ libs/filename_utilities.hpp
@@ -0,0 +1,37 @@
+/*
+  QSTEM - image simulation for TEM/STEM/CBED
+  Copyright (C) 2000-2010  Christoph Koch
+  Copyright (C) 2010-2013  Christoph Koch, Michael Sarahan
+  
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <vector>
+#include <string>
+#include <boost/filesystem.hpp>
+
+std::string AddPositionToFilename(const std::string &label, const std::vector<unsigned> &position)
+{
+  std::stringstream filename, paramname;
+  filename<<label;
+  
+  for (unsigned idx=0; idx<position.size(); idx++)
+    {
+      filename<<"_"<<idx;
+    }
+  
+  filename<<".img";
+  return filename.str();
+}
+
